1a161b72b408ffc110df606124d77ae6bbd9038b
Alexandre Paprocki <alex.paprocki@gmail.com>
2021-03-05 10:41:29 +0100

    little fix

--- front/src/app/user.service.ts
+++ front/src/app/user.service.ts
@@ -11,20 +11,16 @@ export class UserService {
   constructor(private httpClient: HttpClient) {}
 
   register(request: UserRequest) {
-    return this.httpClient.post<UserRequest>(
-      'http://localhost:8080/register',
-      request,
-      {}
-    );
+    return this.httpClient.post('http://localhost:8080/register', request);
   }
 
-  login(request: UserRequest): Observable<any> {
-    return this.httpClient.post(
-      'http://localhost:8080/user/login',
-      request,
-      {}
-    );
-  }
+  // login(request: UserRequest): Observable<any> {
+  //   return this.httpClient.post(
+  //     'http://localhost:8080/user/login',
+  //     request,
+  //     {}
+  //   );
+  // }
 
   getUser(id: string): Observable<User> {
     return this.httpClient.get<User>('http://localhost:8080/user/' + id);
