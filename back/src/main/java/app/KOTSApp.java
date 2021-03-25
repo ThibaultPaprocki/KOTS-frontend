1a4dcdb07a6838307c7f55497716c18031f5a32c
lchauliac <chauliac.ludo@sfr.fr>
2021-03-25 14:00:03 +0100

    liquibase

--- back/src/main/java/app/KOTSApp.java
+++ back/src/main/java/app/KOTSApp.java
@@ -1,26 +1,40 @@
 package app;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.activation.DataSource;
+
+
 @SpringBootApplication
 public class KOTSApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KOTSApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KOTSApp.class, args);
+    }
 
-/*	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200"); //origin à changer lors du déploiement
-			}
-		};
-	}*/
+    /*	@Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**").allowedOrigins("http://localhost:4200"); //origin à changer lors du déploiement
+                }
+            };
+        }*/
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(dataSource());
+        return liquibase;
+    }
+
+    private DataSource dataSource() {
+    }
 
 }
