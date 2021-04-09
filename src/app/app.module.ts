e246e42d9dbcd8e01d46d93400a70e7d7e253a6d
abdullah706 <aaabdal06@gmail.com>
2021-04-09 16:38:26 +0200

    navbar dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { EventComponent } from './event/event.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpRequestInterceptor } from './http.request.interceptor';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -48,3 +49,6 @@ import { HttpRequestInterceptor } from './http.request.interceptor';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+
+
