285007285e617967ec3568b0619411e9b3efd048
Alexandre Paprocki <alex.paprocki@gmail.com>
2021-06-10 08:29:11 +0200

    Wrong fix

--- src/app/admin/control-speedrun/control-speedrun.component.ts
+++ src/app/admin/control-speedrun/control-speedrun.component.ts
@@ -30,7 +30,7 @@ export class ControlSpeedrunComponent implements OnInit {
       const tag = document.createElement("script");
       tag.src = "https://www.youtube.com/iframe_api";
       document.body.appendChild(tag);
-      this.apiLoaded = !this.apiLoaded;
+      this.apiLoaded = true;
     }
   }
 
